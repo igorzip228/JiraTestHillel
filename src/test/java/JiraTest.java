f0f1476026c66087b67fb5f2298b8a87c8f4c9fe
igorzip228 <igorzip228@gmail.com>
2018-06-21 18:56:49 +0300

    Second Commit

--- src/test/java/JiraTest.java
+++ src/test/java/JiraTest.java
@@ -1,22 +1,28 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class JiraTest {
-    WebDriver browser;
+    static WebDriver browser;
     String username = "i.zagorodniy";
     String password = "amarettoqwerty\n";
+    String nameIssue = "My second issue";
+    static String newIssuePath;
 
     @BeforeTest
     public void openChrome() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "/home/qa-1/Загрузки/chromedriver_linux64 (1)/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/home/qa-1/IdeaProjects/chromedriver");
         browser = new ChromeDriver();
         browser.get("http://jira.hillel.it:8080/secure/Dashboard.jspa");
+        browser.manage().window().maximize(); // разворачиваем на весь экран
         sleep(1);
 
     }
@@ -32,26 +38,53 @@ public class JiraTest {
         Thread.sleep(sec * 1000);
     }
 
-    @Test(priority = 1)
+    @Test(description = "Login Jira", priority = 1; )
     public void logInJira() throws InterruptedException {
-        browser.findElement(By.cssSelector("input[name=os_username]")).clear();
-        browser.findElement(By.cssSelector("input[name=os_username]")).sendKeys(username);
-        browser.findElement(By.cssSelector("input[name=os_password]")).clear();
-        browser.findElement(By.cssSelector("input[name=os_password]")).sendKeys(password);
+        clearAndFill(By.cssSelector("input[name=os_username]"), username);
+        clearAndFill(By.cssSelector("input[name=os_password]"), password);
         sleep(5);
-//        Assert.assertEquals(username, browser.findElement(By.cssSelector("img[alt=User profile for Igor Zagorodniy]")).
-//                getText());
-//        sleep(3);
+        Assert.assertEquals("Create", browser.findElement(By.cssSelector("a[id=create_link]")).getText());
+        sleep(3);
     }
 
-    @Test(priority = 2)
+    @Test(description = "Create issue", priority = 2)
     public void createIssue () throws InterruptedException {
         browser.findElement(By.cssSelector("a[id=create_link]")).click();
         sleep(3);
-        browser.findElement(By.cssSelector("input[id=summary]")).sendKeys("My first auto task");
+        clearAndFill(By.cssSelector("input[id=summary]"), nameIssue);
         browser.findElement(By.cssSelector("input[id=create-issue-submit]")).click();
+        sleep(2);
+
+        browser.findElement(By.cssSelector("a[id='find_link']")).click();
+        sleep(2);
+        browser.findElement(By.cssSelector("a[id='filter_lnk_reported_lnk']")).click();
+        sleep(2);
+//        WebElement selectElem = browser.findElement(By.cssSelector("a[id='find_link']"));
+//        Select select = new Select(selectElem);
+//        List options = select.getOptions();
+//        List selectedOptions = select.getAllSelectedOptions();
+//        select.selectByVisibleText("Reported by me");
+        List<WebElement> newIssueLinks = browser.findElements(By.cssSelector("a.splitview-issue-link")); //где взялся селектор???? - общая ссылка для всех элементов со списка
+        Assert.assertTrue(newIssueLinks.size() != 0);
+
+        newIssuePath = newIssueLinks.get(0).getAttribute("href"); // что такое getAttribute
+
+    }
+
+    @Test(description = "Open issue", priority = 3)
+    private void openTicket(){
+        browser.get(newIssuePath);
+        Assert.assertTrue(browser.getTitle().contains(nameIssue));// что такое getTitle()
+    }
 
 
+    private static WebElement clearAndFill(By selector, String data) {
+       WebElement element = browser.findElement(selector);
+        // WebElement element = browser.findElement(selector);
+        element.clear();
+        element.sendKeys(data);
+
+        return element;
     }
 }
 
--- src/test/java/JiraTestRobert.java
+++ src/test/java/JiraTestRobert.java
@@ -1,43 +1,43 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
-public class JiraTestRobert {
-    static WebDriver browser;
-
-    static String baseURL = "http://jira.hillel.it:8080/";
-    static String username = "i.zagorodniy";
-    static String password = "amarettoqwerty\n";
-
-    static String newIssueSummary = "AutoTest" + new SimpleDateFormat("yyyy.MM.dd.MW.mm.ss").format(new Date());
-    static String newIssuePath;
-
-    @BeforeTest
-    protected static void setUp(){
-
-        System.setProperty("webdriver.chrome.driver", "/home/qa-1/Загрузки/chromedriver_linux64 (1)/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized", "--incognito");
-        browser = new ChromeDriver(options);
-        browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        browser.get(baseURL);
-    }
-
-    @Test(description = "Valid Login")
-    private void login() {
-        clearAndFill(By.cssSelector("input#login-form-username"), username);
-        clearAndFill(By.cssSelector("input#login-form-username"), password).submit();
-
-        Assert.assertEquals(username,|
-        browser.findElement(By.cssSelector("a#header-details-fullname")).getAttribute("data-username"));
-    }
-
-}
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import org.testng.Assert;
+//import org.testng.annotations.BeforeTest;
+//import org.testng.annotations.Test;
+//
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//import java.util.concurrent.TimeUnit;
+//
+//public class JiraTestRobert {
+//    static WebDriver browser;
+//
+//    static String baseURL = "http://jira.hillel.it:8080/";
+//    static String username = "i.zagorodniy";
+//    static String password = "amarettoqwerty\n";
+//
+//    static String newIssueSummary = "AutoTest" + new SimpleDateFormat("yyyy.MM.dd.MW.mm.ss").format(new Date());
+//    static String newIssuePath;
+//
+//    @BeforeTest
+//    protected static void setUp(){
+//
+//        System.setProperty("webdriver.chrome.driver", "/home/qa-1/Загрузки/chromedriver_linux64 (1)/chromedriver");
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--start-maximized", "--incognito");
+//        browser = new ChromeDriver(options);
+//        browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//        browser.get(baseURL);
+//    }
+//
+//    @Test(description = "Valid Login")
+//    private void login() {
+//        clearAndFill(By.cssSelector("input#login-form-username"), username);
+//        clearAndFill(By.cssSelector("input#login-form-username"), password).submit();
+//
+//        Assert.assertEquals(username,|
+//        browser.findElement(By.cssSelector("a#header-details-fullname")).getAttribute("data-username"));
+//    }
+//
+//}
